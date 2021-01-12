630b79276f8866808a3b9f24fbc409a01abdd43d
clowns123 <clowns1232@gmail.com>
2021-01-12 23:48:40 +0900

    dev : NewsView component styling end

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <ul class="news-list">
-      <li v-for="(item, index) in news" :key="index">
-        <a :href="item.url">{{ item.title }}</a>
+      <li v-for="(item, index) in news" :key="index" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a :href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${item.user}`">{{
+              item.user
+            }}</router-link>
+          </small>
+        </div>
+        <!-- 
         <small
           >{{ item.time_ago }} by
           <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-        </small>
+        </small> -->
       </li>
     </ul>
   </div>
@@ -17,7 +33,7 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState({
-      news: state => state.news,
+      news: (state) => state.news,
     }),
   },
   created() {
@@ -30,5 +46,29 @@ export default {
 .news-list {
   margin: 0;
   padding: 0;
+}
+
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+
+.news-title {
+  margin: 0;
+}
+
+.link-text {
+  color: #828282;
 }
 </style>
